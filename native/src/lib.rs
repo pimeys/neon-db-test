b1882e5c8230dcd485464f462bea4935a0b4a1f8
Julius de Bruijn <bruijn@prisma.io>
2020-03-18 10:59:49 +0100

    Clean up the code

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,50 +1,74 @@
 use neon::prelude::*;
 use quaint::{pooled::Quaint, prelude::*};
-use std::sync::Arc;
+use std::{
+    io::{Error, ErrorKind},
+    sync::Arc,
+};
 use tokio::runtime::Runtime;
 
+pub struct InnerClient {
+    pool: Quaint,
+}
+
+impl InnerClient {
+    pub async fn new(url: &str) -> anyhow::Result<Self> {
+        Ok(Self {
+            pool: Quaint::new(url).await?,
+        })
+    }
+
+    pub async fn select_1(&self) -> anyhow::Result<i64> {
+        let conn = self.pool.check_out().await?;
+        let res = conn.query_raw("SELECT 1", &[]).await?;
+
+        let row = res.into_single()?;
+        let val = row.into_single()?;
+
+        val.as_i64()
+            .ok_or(Error::new(ErrorKind::InvalidData, "Not an integer.").into())
+    }
+}
+
 pub struct Client {
-    pool: Arc<Quaint>,
+    inner: Arc<InnerClient>,
     runtime: Arc<Runtime>,
 }
 
 declare_types! {
     pub class DatabaseClient for Client {
         init(mut cx) {
-            let url: String = cx.argument::<JsString>(0).unwrap().value();
+            let url: String = cx.argument::<JsString>(0)?.value();
             let mut runtime = Runtime::new().unwrap();
 
-            let pool = runtime.block_on(async {
-                Quaint::new(&url).await
+            let inner = runtime.block_on(async {
+                InnerClient::new(&url).await
             }).unwrap();
 
             Ok(Client {
-                pool: Arc::new(pool),
+                inner: Arc::new(inner),
                 runtime: Arc::new(runtime),
             })
         }
 
         method select(mut cx) {
             let this = cx.this();
-            let func = cx.argument::<JsFunction>(0).unwrap();
+            let func = cx.argument::<JsFunction>(0)?;
 
-            let (pool, runtime) = {
+            let (inner, runtime) = {
                 let guard = cx.lock();
                 let client = this.borrow(&guard);
 
-                (client.pool.clone(), client.runtime.clone())
+                (client.inner.clone(), client.runtime.clone())
             };
 
             let cb = EventHandler::new(&cx, this, func);
 
             runtime.spawn(async move {
                 let result: anyhow::Result<()> = Ok(());
-                let conn = pool.check_out().await.unwrap();
-                let res = conn.query_raw("SELECT 1", &[]).await.unwrap();
+                let num = inner.select_1().await?;
 
                 cb.schedule(move |cx| {
-                    let result = res.into_single().unwrap()[0].as_i64().unwrap();
-                    vec![cx.number(result as f64)]
+                    vec![cx.number(num as f64)]
                 });
 
                 result
